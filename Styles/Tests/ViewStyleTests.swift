1632da8c281b5a22d6409b43921ddb8eae6ce9b8
Jakub Petrík <petrik@inloop.eu>
2018-02-26 21:14:46 +0100

    FEAT: ViewStyle combine and update tests

--- Styles/Tests/ViewStyleTests.swift
+++ Styles/Tests/ViewStyleTests.swift
@@ -3,7 +3,7 @@
 import XCTest
 @testable import Styles
 
-final class UIViewColorStyleTests: XCTestCase {
+final class ViewStyleTests: XCTestCase {
     let colorStyle = ViewStyle(.backgroundColor(.red))
     let tintStyle = ViewStyle(.tintColor(.blue))
     let borderColorStyle = ViewStyle(.borderColor(.magenta))
@@ -11,58 +11,67 @@ final class UIViewColorStyleTests: XCTestCase {
     let opacityStyle = ViewStyle(.opacity(0.3))
     let roundCornersStyle = ViewStyle(.roundCorners(.allCorners, radius: 10))
 
-    func testVeiewHasProperBackgoundColor() {
-        let view = UIView()
-        XCTAssertTrue(view.backgroundColor == nil)
+    var view: UIView!
 
+    override func setUp() {
+        super.setUp()
+        view = UIView()
+    }
+
+    func testBackgoundColor() {
         view.viewStyle = colorStyle
-        XCTAssertTrue(view.backgroundColor != nil)
+
         XCTAssertTrue(view.backgroundColor == .red)
     }
 
-    func testViewHasTintIfStyleApplied() {
-        let view = UIView()
+    func testTint() {
         view.viewStyle = tintStyle
 
         XCTAssertTrue(view.tintColor == .blue)
     }
 
-    func testHasBorderColorIfStyleApplied() {
-        let view = UIView()
+    func testBorderColor() {
         view.viewStyle = borderColorStyle
 
         XCTAssertTrue(view.layer.borderColor == UIColor.magenta.cgColor)
     }
 
-    func testHasBorderWidthIfStyleApplied() {
-        let view = UIView()
+    func testBorderWidth() {
         view.viewStyle = borderWidthStyle
 
         XCTAssertTrue(view.layer.borderWidth == 10)
     }
 
-    func testHasProperOpacityIfStyleApplied() {
-        let view = UIView()
+    func testOpacity() {
         view.viewStyle = opacityStyle
 
         XCTAssertTrue(view.layer.opacity == 0.3)
     }
 
-    func testHasCornerRadiusesIfStyleApplied() {
-        let view = UIView()
+    func testCornerRadius() {
         view.viewStyle = roundCornersStyle
-        let fullMask: CACornerMask = [
-            .layerMinXMinYCorner,
-            .layerMaxXMinYCorner,
-            .layerMinXMaxYCorner,
-            .layerMaxXMaxYCorner
-        ]
+
         if #available(iOS 11.0, *) {
             XCTAssertTrue(view.clipsToBounds)
             XCTAssertTrue(view.layer.cornerRadius == 10)
-            XCTAssertTrue(view.layer.maskedCorners == fullMask)
+            XCTAssertTrue(view.layer.maskedCorners == UIRectCorner.allCorners.maskedCorners)
         } else {
             XCTAssertTrue(view.layer.mask != nil)
         }
+    }
+
+    func testCombining() {
+        let combinedStyle = colorStyle + tintStyle
+        view.viewStyle = combinedStyle
+
+        XCTAssert(view.backgroundColor == .red)
+        XCTAssert(view.tintColor == .blue)
+    }
+
+    func testUpdating() {
+        let updatedStyle = colorStyle.updating(.backgroundColor(.green))
+        view.viewStyle = updatedStyle
+
+        XCTAssert(view.backgroundColor == .green)
     }
 }
