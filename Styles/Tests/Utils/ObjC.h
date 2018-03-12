2a4055a42f17a6be32dadc2510c5dc0e695461f8
Jakub Petrík <petrik@inloop.eu>
2018-03-12 20:00:44 +0100

    FIX: Remove objc from pod

--- Styles/Tests/Utils/ObjC.h
+++
@@ -1,7 +0,0 @@
-//  source: https://stackoverflow.com/questions/32758811/catching-nsexception-in-swift
-
-#import <Foundation/Foundation.h>
-
-@interface ObjC : NSObject
-+ (BOOL)catchException:(void(^)(void))tryBlock error:(__autoreleasing NSError **)error;
-@end
