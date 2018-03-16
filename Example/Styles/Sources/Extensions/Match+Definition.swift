9912eaf1ac083bf85cdbe7926d982b6f1aebc38c
Radim Halfar <radim.halfar@inloop.eu>
2018-03-16 14:02:25 +0100

    FEAT: Text effects sample app
    Closes #60

---
+++ Example/Styles/Sources/Extensions/Match+Definition.swift
@@ -0,0 +1,8 @@
+//  Copyright © 2018 Inloop, s.r.o. All rights reserved.
+
+import Styles
+
+struct MatchDefinition {
+	let match: Match
+	let matchDescription: String
+}
---
+++ Example/Styles/Sources/Extensions/TextEffect+Definition.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2018 Inloop, s.r.o. All rights reserved.
+
+import Styles
+
+extension TextEffect {
+	struct Definition {
+		let effect: TextEffect
+		let effectDescription: String
+		let textStyleDescription: String
+	}
+}
