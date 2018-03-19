cc20d4c39c99707008586543b072378d227f58ae
Jakub Petrík <petrik@inloop.eu>
2018-03-19 16:21:42 +0100

    DOCS: Add Range documentation

--- Styles/Classes/Matches/Range.swift
+++ Styles/Classes/Matches/Range.swift
@@ -7,25 +7,47 @@ private enum RangeType {
     case swift(Swift.Range<String.Index>)
 }
 
-public final class Range<T>: NSObject, Match {
+/**
+ Range is a convenience class that represents `Match` with concrete ranges.
+ Useful for labels with hardcoded strings.
+ @see `Match`
+*/
+public final class Range: NSObject, Match {
     private let ranges: [RangeType]
 
+    /// Intialize `Range` with location and length.
+    ///
+    /// - Parameters:
+    ///   - location: starting location
+    ///   - length: length of the range
     public convenience init(location: Int, length: Int) {
         self.init(ranges: [.foundation(NSRange(location: location, length: length))])
     }
 
+    /// Intialize `Range` with `NSRange`.
+    ///
+    /// - Parameter range: range to be matched
     public convenience init(_ range: NSRange) {
         self.init(ranges: [.foundation(range)])
     }
 
+    /// Intialize `Range` with multiple `NSRange`s.
+    ///
+    /// - Parameter ranges: ranges to be matched
     public convenience init(_ ranges: [NSRange]) {
         self.init(ranges: ranges.map { .foundation($0) })
     }
 
+    /// Intialize `Range` with `Swift.Range`.
+    ///
+    /// - Parameter range: range to be matched
     public convenience init(_ range: Swift.Range<String.Index>) {
         self.init(ranges: [range].map { .swift($0) })
     }
 
+    /// Intialize `Range` with multiple `Swift.Range`s.
+    ///
+    /// - Parameter ranges: ranges to be matched
     public convenience init(_ ranges: [Swift.Range<String.Index>]) {
         self.init(ranges: ranges.map { .swift($0) })
     }
