d4e983b81ea435254f4786d1d18a922132d6b031
Jakub Petrík <petrik@inloop.eu>
2018-07-30 11:07:38 +0200

    FEAT: Add reference button

--- Example/Styles/Sources/Scenes/TestBed/TestBedViewController.swift
+++ Example/Styles/Sources/Scenes/TestBed/TestBedViewController.swift
@@ -14,6 +14,19 @@ private extension TextStyle {
     )
 }
 
+private extension UIColor {
+    var image: UIImage {
+        let rect = CGRect(origin: .zero, size: CGSize(width: 1, height: 1))
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(cgColor)
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
+
 private extension ViewStyle {
     static let normal = ViewStyle(.backgroundColor(.red))
     static let selected = ViewStyle(.backgroundColor(.green))
@@ -22,17 +35,22 @@ private extension ViewStyle {
 final class TestBedViewController: UIViewController {
     @IBOutlet private var textField: UITextField!
     @IBOutlet private var button: UIButton!
+    @IBOutlet private var plainButton: UIButton!
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
+        setupTextField()
+        setupButton()
+        setupPlainButton()
     }
 
-    func setup() {
+    private func setupTextField() {
         textField.setTextStyle(.active, for: .editing)
         textField.setTextStyle(.inactive, for: .inactive)
+    }
 
+    private func setupButton() {
         print("setting ViewStyle for .normal")
         button.setViewStyle(.normal, for: .normal)
 
@@ -44,7 +62,20 @@ final class TestBedViewController: UIViewController {
 
         print("setting TextStyle for .highlighted")
         button.setTextStyle(.inactive, for: .highlighted)
+    }
 
+    private func setupPlainButton() {
+        plainButton.setBackgroundImage(UIColor.red.image, for: .normal)
+        plainButton.setBackgroundImage(UIColor.green.image, for: .highlighted)
+        let title = "Plain System Button"
+        plainButton.setAttributedTitle(
+            TextStyle.active.apply(to: title),
+            for: .normal
+        )
+        plainButton.setAttributedTitle(
+            TextStyle.inactive.apply(to: title),
+            for: .highlighted
+        )
     }
 
     @objc private func animationDidStop(_ note: Notification) {
